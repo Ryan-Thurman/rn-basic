a1e822ede76dbc049df3af1e52c22c553b1067be
RThurman <Ryan-Thurman@users.noreply.github.com>
2017-11-10 13:43:15 -0800

    added firebase config

--- auth/App.js
+++ auth/App.js
@@ -1,19 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Text,
   View
 } from 'react-native';
 
+import { Header } from './src/components/common'
+
+import firebase from 'firebase';
+import FirebaseConfig from './config/firebase_config'
+
 export default class App extends Component<{}> {
+
+  componentDidMount() {
+    firebase.initializeApp(FirebaseConfig)
+  }
+
   render() {
     return (
       <View>
+        <Header />
         <Text> App is working </Text>
       </View>
     );
