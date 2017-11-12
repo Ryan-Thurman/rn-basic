d031961ba3d5e8349d9ba65f49809a9823d312bb
Ryan Thurman <Ryan-Thurman@users.noreply.github.com>
2017-11-11 16:35:45 -0800

    added Firebase atuh

--- auth/App.js
+++ auth/App.js
@@ -3,17 +3,17 @@ import {
   Text,
   View
 } from 'react-native';
-
-import { Header } from './src/components/common'
-import LoginForm from './src/components/LoginForm'
-
 import firebase from 'firebase';
-import FirebaseConfig from './config/firebase_config'
+
+import { Header } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
+
+import FirebaseConfig from './config/firebase_config';
 
 export default class App extends Component<{}> {
 
   componentDidMount() {
-    firebase.initializeApp(FirebaseConfig)
+    firebase.initializeApp(FirebaseConfig);
   }
 
   render() {
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
-// import {View, TextInput} from 'react-native';
+import {Text} from 'react-native';
+import firebase from 'firebase';
 import {Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends Component {
 		constructor(props) {
 				super(props)
 				this.state = {}
+				this.onButtonPress = this.onButtonPress.bind(this)
 		}
+		onButtonPress() {
+				const {email, password} = this.state;
 
+				firebase
+						.auth()
+						.signInWithEmailAndPassword(email, password)
+						.catch(() => {
+								firebase
+										.auth()
+										.createUserWithEmailAndPassword(email, password)
+										.catch((error) => {
+												this.setState({error: 'Authentication Failed'})
+										})
+						})
+		}
 		render() {
 				return (
 						<Card>
@@ -25,12 +41,13 @@ class LoginForm extends Component {
 												label={'Password'}
 												value={this.state.password}
 												onChangeText={(password) => this.setState({password})}
-												secureTextEntry={true}
-												/>
+												secureTextEntry={true}/>
 								</CardSection>
-
+								<Text>
+										{this.state.error}
+								</Text>
 								<CardSection>
-										<Button>
+										<Button onPress={this.onButtonPress}>
 												Log In
 										</Button>
 								</CardSection>
