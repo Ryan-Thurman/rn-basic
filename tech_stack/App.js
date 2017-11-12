fab80578c7d6855bb9dee81827e07b13df2685ae
Ryan Thurman <Ryan-Thurman@users.noreply.github.com>
2017-11-11 19:44:33 -0800

    folder fix

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -13,12 +13,14 @@ import {
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers'
+import { Header } from './src/components/common'
 
 export default class App extends Component<{}> {
   render() {
     return (
 			<Provider store={createStore(reducers, {})}>
       <View >
+				<Header />
         <Text >
           Welcome to React Native!
         </Text>
