42f4d1d7f16218b161d3fccaf88915d9eaad64fb
Ryan Thurman <Ryan-Thurman@users.noreply.github.com>
2017-11-12 10:37:04 -0800

    removed rn boilerplate

--- manager/App.js
+++ manager/App.js
@@ -1,55 +1,28 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux'
 import firebase from 'firebase'
+import ReduxThunk from 'redux-thunk'
 
 import reducers from './src/reducers'
 import Firebase_config from './config/Firebase_config'
 
 import LoginForm from './src/components/LoginForm'
+import {Header} from './src/components/common'
 
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-	componentWillMount() {
-		firebase.initializeApp(Firebase_config);
-	}
-  render() {
-    return (
-			<Provider store={createStore(reducers)}>
-					<LoginForm />
-			</Provider>
-    );
-  }
+export default class App extends Component < {} > {
+		componentWillMount() {
+				firebase.initializeApp(Firebase_config);
+		}
+		render() {
+				return (
+						<Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+								<View>
+										<Header headerText={'manager'}/>
+										<LoginForm/>
+								</View>
+						</Provider>
+				)
+		}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
