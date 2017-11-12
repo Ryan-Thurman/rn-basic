c20060bc484678f5f0ba7fef1f515651c0262c8a
Ryan Thurman <Ryan-Thurman@users.noreply.github.com>
2017-11-12 09:03:23 -0800

    basic redux added with animation

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -14,19 +14,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers'
 import { Header } from './src/components/common'
+import LibraryList from './src/components/LibraryList'
 
 export default class App extends Component<{}> {
   render() {
     return (
 			<Provider store={createStore(reducers, {})}>
-      <View >
-				<Header />
-        <Text >
-          Welcome to React Native!
-        </Text>
-        <Text>
-          To get started, edit App.js
-        </Text>
+      <View style={{ flex: 1 }}>
+				<Header headerText={'Tech Stack'}/>
+        <LibraryList />
       </View>
 			</Provider>
     );
